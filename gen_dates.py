37ae6458eae3b37d381cc634e5920d4e3328fdd1
Jon Craton <jncraton@gmail.com>
2023-05-20 16:32:00 -0400

    Show weeks

--- gen_dates.py
+++ gen_dates.py
@@ -22,7 +22,18 @@ for b in config["breaks"]:
     rules.exdate(datetime.fromisoformat(b))
 
 with open('topics.tsv') as f:
-    lectures = [r for r in csv.DictReader(f, dialect='excel-tab') if r['type'] == 'lecture']
+    lectures = [r for r in csv.DictReader(f, dialect='excel-tab') if r['type'] != 'lab']
+
+    start_week = None
+    last_week = 0
 
     for i, day in enumerate(zip_longest(rules, lectures)):
-        print(f"{i+1:02d}. {day[1]['name'] if day[1] != None else 'Out of topics'} ({day[0].strftime('%A, %B %d')})")
+        if not start_week:
+            start_week = int(day[0].strftime("%V")) - 1
+
+        week = int(day[0].strftime("%V")) - start_week
+
+        if week != last_week:
+            last_week = week
+        
+        print(f"Week {week} Day {i+1:02d} {day[1]['name'] if day[1] != None else 'Out of topics'} ({day[0].strftime('%A, %B %d')})")
